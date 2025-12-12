b5c6fe0f059bc799c1c511c2e71236fcd741914b
DavidKra-dev <108519803+Yeshihiro1510@users.noreply.github.com>
2025-12-12 21:30:58 +0300

    Burger constructor

---
+++ Assets/Project/Source/BurgerConstructor.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BurgerConstructor : MonoBehaviour
+{
+    private void Start()
+    {
+        ThoseMethod(out var myBurger, "cutlet", "cutlet", "special sauce", "sesame bun");
+        Debug.Log($"Your burger is {myBurger}");
+    }
+
+    private void ThoseMethod(out string burger, params string[] ingredients)
+    {
+        int cucumbers = 0;
+        int cheese = 0;
+        int ketchup = 0;
+        int cutlets = 0;
+
+        int specialSauce = 0;
+        int sesameBun = 0;
+
+        foreach (var i in ingredients)
+        {
+            if (i == "cucumber") cucumbers++;
+            if (i == "cheese") cheese++;
+            if (i == "ketchup") ketchup++;
+            if (i == "cutlet") cutlets++;
+            if (i == "special sauce") specialSauce++;
+            if (i == "sesame bun") sesameBun++;
+        }
+
+        if (cucumbers == 1 && cheese == 1 && ketchup == 1 && cutlets == 1) burger = "cheeseburger";
+        else if (specialSauce == 1 && sesameBun == 1 && cutlets == 2) burger = "big mac";
+        else burger = "random shi";
+    }
+}
